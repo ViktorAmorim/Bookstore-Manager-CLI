4e55d74a89fb7aa422ddfd223d67c03740cb408b
ViktorAmorim <joao.victor.amorim.martins@hotmail.com>
2026-07-07 20:16:01 -0300

    feat: adicionamos a funcionalidade delete para autores e atualizamos o menu de opções

--- src/repositories/AutorRepository.ts
+++ src/repositories/AutorRepository.ts
@@ -1,23 +1,29 @@
-import pool from '../database/connection';
-import { Autor } from '../models';
+import pool from "../database/connection";
+import { Autor } from "../models";
 
 export class AutorRepository {
   async cadastrar(autor: Autor): Promise<Autor> {
-    const query = 'INSERT INTO autores (nome, nacionalidade) VALUES ($1, $2) RETURNING *';
+    const query =
+      "INSERT INTO autores (nome, nacionalidade) VALUES ($1, $2) RETURNING *";
     const values = [autor.nome, autor.nacionalidade];
     const { rows } = await pool.query(query, values);
     return rows[0];
   }
 
   async listar(): Promise<Autor[]> {
-    const query = 'SELECT * FROM autores ORDER BY id ASC';
+    const query = "SELECT * FROM autores ORDER BY id ASC";
     const { rows } = await pool.query(query);
     return rows;
   }
 
   async buscarPorId(id: number): Promise<Autor | null> {
-    const query = 'SELECT * FROM autores WHERE id = $1';
+    const query = "SELECT * FROM autores WHERE id = $1";
     const { rows } = await pool.query(query, [id]);
     return rows.length ? rows[0] : null;
+  }
+
+  async deletarPorId(id: number): Promise<void> {
+    const query = "DELETE FROM autores WHERE id = $1";
+    await pool.query(query, [id]);
   }
 }
--- src/services/AutorService.ts
+++ src/services/AutorService.ts
@@ -1,5 +1,5 @@
-import { AutorRepository } from '../repositories/AutorRepository';
-import { Autor } from '../models';
+import { AutorRepository } from "../repositories/AutorRepository";
+import { Autor } from "../models";
 
 export class AutorService {
   private autorRepository: AutorRepository;
@@ -9,13 +9,13 @@ export class AutorService {
   }
 
   async cadastrarAutor(nome: string, nacionalidade?: string): Promise<Autor> {
-    if (!nome || nome.trim() === '') {
-      throw new Error('O nome do autor é obrigatório.');
+    if (!nome || nome.trim() === "") {
+      throw new Error("O nome do autor é obrigatório.");
     }
 
-    return await this.autorRepository.cadastrar({ 
-      nome: nome.trim(), 
-      nacionalidade: nacionalidade?.trim() 
+    return await this.autorRepository.cadastrar({
+      nome: nome.trim(),
+      nacionalidade: nacionalidade?.trim(),
     });
   }
 
@@ -29,5 +29,13 @@ export class AutorService {
       throw new Error(`Autor com ID ${id} não encontrado.`);
     }
     return autor;
+  }
+
+  async deletarAutorPorId(id: number): Promise<void> {
+    const autor = await this.autorRepository.buscarPorId(id);
+    if (!autor) {
+      throw new Error(`Autor com ID ${id} não encontrado.`);
+    }
+    await this.autorRepository.deletarPorId(id);
   }
 }
